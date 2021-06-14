4ad0d385c5375cd9ad0c2d63b4306496471d6117
Kwarn <karlwarner.dev@gmail.com>
2021-06-14 15:45:14 +0100

    AWS S3 multiple image upload now correctly sends response data with uploaded image location URLS

--- aws/s3.js
+++ aws/s3.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 const S3 = require('aws-sdk/clients/s3');
 const path = require('path');
-require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 
+require('dotenv').config();
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
 const accessKey = process.env.AWS_ACCESS_KEY;
@@ -17,8 +17,6 @@ AWS.config.update({
   secretAccessKey: secretAccessKey,
   region: region,
 });
-
-console.log(`process.env.bucketName`, process.env);
 
 const s3bucket = new S3({
   region,
@@ -47,37 +45,36 @@ const storage = multer.memoryStorage({
 const multipleUpload = multer({
   storage: storage,
   fileFilter: fileFilter,
-}).array('images', 10);
+}).array('images');
+
 const upload = multer({ storage: storage, fileFilter: fileFilter }).single(
   'file'
 );
 
-router.post('/post-images', multipleUpload, function (req, res) {
-  const files = req.files;
-  var ResponseData = [];
+const createParams = async files => {};
 
-  files.map(item => {
-    var params = {
-      Bucket: bucketName,
-      Key: item.originalname,
-      Body: item.buffer,
-    };
-    console.log(params);
-    s3bucket.upload(params, function (err, data) {
-      if (err) {
-        res.json({ error: true, Message: err });
-      } else {
-        ResponseData.push(data);
-        console.log(ResponseData);
+router.post('/post-images', multipleUpload, async function (req, res, next) {
+  const resData = [];
+
+  for (const file of req.files) {
+    try {
+      const params = {
+        Bucket: bucketName,
+        Key: file.originalname,
+        Body: file.buffer,
+      };
+      const uploaded = await s3bucket.upload(params).promise();
+      resData.push(uploaded.Location);
+
+      if (resData.length === req.files.length) {
+        res.status(202).json({
+          error: false,
+          body: resData,
+        });
       }
-    });
-  });
-  if (ResponseData.length == files.length) {
-    res.json({
-      error: false,
-      Message: 'File/s Uploaded',
-      Data: ResponseData,
-    });
+    } catch (err) {
+      console.log(`err`, err);
+    }
   }
 });
 
